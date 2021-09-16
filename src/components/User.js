acaa155490c10e23463b0f1a99452325104f5a36
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-09-16 11:31:28 +0500

    adding first class based components

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,18 @@
+import { Component } from 'react';
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+
+// class components
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+
+// Functional Components
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
